7dfc1d4d032d099af44613493625b62a2bbb5a3c
Anthony Chu <antchu@microsoft.com>
2019-04-17 23:35:45 -0700

    Add missing props on delete & highlight transition

--- EventGridTrigger/run.ps1
+++ EventGridTrigger/run.ps1
@@ -1,36 +1,23 @@
 param($eventGridEvent, $TriggerMetadata)
 
-# Make sure to pass hashtables to Out-String so they're logged correctly
-#$eventGridEvent | ConvertTo-Json 
-
 $resourceId = $eventGridEvent.subject
-$operationName = $eventGridEvent.data.operationName
-$username = $eventGridEvent.data.claims["name"]
 $eventType = $eventGridEvent.eventType
-$timestamp = $eventGridEvent.eventTime
 
 if ($eventType -ne "Microsoft.Resources.ResourceDeleteSuccess") {
     $resource = Get-AzResource -ResourceId $resourceId
-
-    $resourceName = $resource.ResourceName
-    $resourceGroupName = $resource.ResourceGroupName
-    $location = $resource.Location
-    $kind = $resource.Kind
 }
-
-#Write-Host "$($kind) $($location) $($operationName) $($username) $($eventType) $($timestamp)"
 
 Push-OutputBinding -Name SignalRMessages -Value (@{
     Target = 'newResourceEvent'
     Arguments = , @{
-        resourceGroupName = $resourceGroupName
-        resourceName = $resourceName
+        resourceGroupName = $resource.ResourceGroupName
+        resourceName = $resource.ResourceName
         resourceId = $resourceId
-        kind = $kind
-        location = $location
-        timestamp = $timestamp
-        operationName = $operationName
-        username = $username
-        eventType = $eventType
+        resourceProvider = $eventGridEvent.data.resourceProvider
+        location = $resource.Location
+        timestamp = $eventGridEvent.eventTime
+        operationName = $eventGridEvent.data.operationName
+        username = $eventGridEvent.data.claims["name"]
+        eventType = $eventGridEvent.eventType
     }
 })
